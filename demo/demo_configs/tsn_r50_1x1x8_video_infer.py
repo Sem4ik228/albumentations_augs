fdc8e4c968d40dbed3edd10b5cd1cc7ef38ea3e9
Семён Барыльник <s-barylnik@office.iss>
2024-06-03 16:49:12 +0300

    add InverseSparkles

--- demo/demo_configs/tsn_r50_1x1x8_video_infer.py
+++ demo/demo_configs/tsn_r50_1x1x8_video_infer.py
@@ -14,12 +14,12 @@ test_pipeline = [
     dict(type='DecordDecode'),
     dict(type='Resize', scale=(-1, 256)),
     dict(type='CenterCrop', crop_size=224),
-    dict(type='RandomSunFlare', src_radius=100, src_colors=[(0, 0, 0), (255, 255, 255)], p=0.0),
-    dict(type='RandomSnow', snow_point_lower=0.1, snow_point_upper=0.3, brightness_coeff=(0.5, 2.5), p=0.0),
-    dict(type='PitchAndTwirl', severity=(5.5, 10.0), p=0.0),
-    dict(type='Sparkles', severity=(1.0, 6.0), p=0.0),
-    dict(type='RandomGrayWithCompression', quality_lower=30, quality_upper=95, p=0.0),
-    dict(type='InverseSparkles', p=1.0),
+    dict(type='RandomSunFlare', src_radius=100, src_colors=[(0, 0, 0), (255, 255, 255)], p=1.0),
+    dict(type='RandomSnow', snow_point_lower=0.1, snow_point_upper=0.3, brightness_coeff=(0.5, 2.5), p=1.0),
+    dict(type='PitchAndTwirl', severity=(5.5, 10.0), p=1.0),
+    dict(type='Sparkles', severity=(1.0, 6.0), p=1.0),
+    dict(type='RandomGrayWithCompression', quality_lower=30, quality_upper=95, p=1.0),
+    dict(type='InverseSparkles', severity=(1.0, 10.0), color=[[255, 255, 255], [0, 0, 0]], p=1.0),
     dict(type='FormatShape', input_format='NCHW'),
     dict(type='PackActionInputs')
 ]
